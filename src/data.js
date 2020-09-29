ad82acda6ed4d247689a1a63ce6a3fff46de29f6
Elton Santos <eltonaxl@hotmail.com>
2020-09-29 11:34:23 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Elton Santos',
+        username: 'eltin182'
+    },
+    {
+        id: 2,
+        name: 'Ericson Santos',
+        username: 'Raynor'
+    }
+];
+
+export default userList;
